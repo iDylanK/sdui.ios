5c4bb1cd1943f06d91dda27a310ce7cb8617bd78
iDylanK <dylankieft@live.nl>
2022-06-22 14:31:06 +0200

    Added validation project

---
+++ SDUIValidation/Shared/SDUIValidationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDUIValidationApp.swift
+//  Shared
+//
+//  Created by Dylan on 22/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct SDUIValidationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
